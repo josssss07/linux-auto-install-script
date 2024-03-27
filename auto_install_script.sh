76a251909f4e7dac525c23dd5acc8532f43f523b
jossssss07 <joshuadesai03@gmail.com>
2024-03-27 15:28:12 +0530

    added some interaction

--- auto_install_script.sh
+++ auto_install_script.sh
@@ -24,27 +24,92 @@
 #15-R and r-studio 
 
 #update system
+echo "updating system"
 sudo apt update
+echo "update complate"
 
+echo "installing snap"
 sudo apt install snapd -y
+echo "snap installed"
+
+echo "Installing vs code"
 sudo snap install --classic code
+echo "vs code installed"
+
+echo "installing eclipse"
 sudo snap install eclipse --classic 
-sudo snap install discord 
+echo "eclipse installed"
+
+echo "installing discord" 
+sudo snap install discord
+echo "discord insatlled" 
+
+echo "installing android studio"
 sudo snap install android-studio --classic
+echo "android studio installed"
+
+echo "installing pycharm" 
 sudo snap install pycharm-community --classic
+echo "pycharm installed"
+
+echo "installing intillij idea" 
 sudo snap install  intellij-idea-community --classic
+echo "intillij installed"
+
+echo "installing flutter"
 sudo snap install flutter --classic 
+echo "updating path "
 flutter sdk-path 
+echo "flutter installed, please check android licenses"
 #please download android sdk from android studio manually and then run 
 #flutter doctor 
+echo "installing hollywood and neofetch"
 sudo apt install hollywood -y 
 sudo apt install neofetch -y 
+echo "neofetch and hollywood installed"
+
+echo "installing java (openjdk-19)" 
 sudo apt install openjdk-19-jre-headless -y 
+echo "java installed" 
+
+echo "installing npm"
 sudo apt install npm
-sudo snap install spotify 
+echo "npm installed"
+
+echo "installing spotify"
+sudo snap install spotify
+echo "spotify installed "
+
+echo "installing beekeeper" 
 sudo snap install beekeeper-studio --classic
+echo "beekeeper installed"
+
+echo "verifying python"
 sudo apt install python3 -y
+echo "python verified, installing nginx"
 sudo apt install nginx
+echo "nginx installed, installing r" 
 sudo apt-get install r-base
+echo "r installed"
 #to instal r studio you need to go to the official r studio website 
 #and install based on ur version 
+
+sleep 1
+echo "Install script completed, rebooting to complete any incomplete tasks"
+sleep 1
+
+echo "####################    Rebooting    #######################"
+echo "Rebooting in 5     Press Ctrl + C to cancel"
+sleep 1
+echo "Rebooting in 4     Press Ctrl + C to cancel"
+sleep 1
+echo "Rebooting in 3     Press Ctrl + C to cancel"
+sleep 1
+echo "Rebooting in 2     Press Ctrl + C to cancel"
+sleep 1
+echo "Rebooting in 1     Press Ctrl + C to cancel"
+sleep 1
+echo "Rebooting in 0     Press Ctrl + C to cancel"
+echo "####################    Rebooting    #######################"
+
+sudo reboot now
